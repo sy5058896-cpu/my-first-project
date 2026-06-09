021feb6e618565376b778f7146e0bf189e346d57
Shashi kumar yadav <sy5058896@gmail.com>
2026-06-09 12:01:12 +0530

    initial commit

---
+++ cal.py
@@ -0,0 +1,15 @@
+# addition
+def addl(a):
+    c = 0
+    for i in a:
+        c = c +i
+    return c
+def add(a,b):
+    c = a+b
+    return c
+def multi(a,b):
+    c = a*b
+    return c
+a = [1,2,3,4]
+h = addl(a)
+print(h)
