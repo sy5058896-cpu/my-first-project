0821c77a2efd8b86465fce5deba75c8138cd8b9e
Shashi kumar yadav <sy5058896@gmail.com>
2026-06-09 12:38:34 +0530

    2ndcommit

--- cal.py
+++ cal.py
@@ -1,4 +1,4 @@
-# addition
+# this is casic basic calculator ,addl,sub,add,multiplication
 def addl(a):
     c = 0
     for i in a:
@@ -7,9 +7,19 @@ def addl(a):
 def add(a,b):
     c = a+b
     return c
+def sub(a,b):
+    c = a-b
+    return c
 def multi(a,b):
     c = a*b
     return c
 a = [1,2,3,4]
 h = addl(a)
+p = add(9,5)
+c = sub(7,5)
+f = multi(6,5)
 print(h)
+print(p)
+print(f)
+print(c)
+
